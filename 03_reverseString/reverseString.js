f90872f93c8e01767f38dcb8c8a3b60b7256ad36
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-04-10 16:13:27 +0400

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,16 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // We take the length of the string - 1 as index starts at 0
+    let lengthString = string.length - 1;
+
+    let stringSplittedArray = string.split("")
+    let reversedArray = [];
+
+    for (let i = 0; i <= lengthString; i++) {
+        let reversedIndex = lengthString - i
+        reversedArray[i] = stringSplittedArray[reversedIndex]
+        console.log(i)
+    }
+    return reversedArray.join("")
 
 };
 
