4acf920ee017dc9c9756d6215a5faee186a415e9
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-04-11 11:26:33 +0400

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(integerFirst, integerLast) {
+    let sum;
+    let counter;
 
+    if (integerFirst > integerLast) {
+        sum = integerLast;
+        counter = integerLast
+        while (counter < integerFirst) {
+            counter++;
+            sum += counter;
+        }
+    }
+    else if (typeof integerFirst != "number" || typeof integerLast != "number" || integerFirst < 0 || integerLast < 0) {
+        return ("ERROR")
+    }
+    else {
+        sum = integerFirst;
+        counter = integerFirst;    while (counter < integerLast) {
+            counter++;
+            sum += counter;
+        }
+        while (counter < integerLast) {
+            counter++;
+            sum += counter;
+        }
+    }
+
+
+    return sum;
 };
 
 // Do not edit below this line
