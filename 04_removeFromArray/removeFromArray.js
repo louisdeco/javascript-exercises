226fa7cfe2c3f549510ec0775396e0afc33f4a03
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-04-11 11:05:12 +0400

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...elementsToRemove) {
+    for (const element of elementsToRemove) {
+        for (index = 0; index < array.length; index++) {
+            let numberInArray = array[index];
+            if (numberInArray === element) {
+                array.splice(index, 1);
+                index--;
+            }
+        }
+    }
+    console.log(array)
+    return array;
 };
 
 // Do not edit below this line
